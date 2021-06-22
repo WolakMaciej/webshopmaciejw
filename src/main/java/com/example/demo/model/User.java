0479fb54089a9ba4661b7615cd7c75876c0d4925
Maciej Wolak <74461485+WolakMaciej@users.noreply.github.com>
2021-06-22 11:43:48 +0200

    Update User.java

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -13,7 +13,7 @@ import java.util.Collections;
 
 @Data
 @Entity
-@Table(name="\"user\"")
+@Table(name="`users`")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
